64304102348ee7a7cb6417cc579b54f6924492cc
moalemi <mohammadmoallemi78@gmail.com>
2024-11-02 09:38:13 +0330

    feat(shared/constants): add apiUrl constants

---
+++ src/shared/constants/apiUrls.ts
@@ -0,0 +1,4 @@
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+
+export const CONTACT_FORM_URL = `${API_BASE_URL}/api/forms/contact`;
+export const ORDER_FORM_URL = `${API_BASE_URL}/api/forms/order`;
