04780357fd4097e9c6b546efa89cb7d287e8a5e1
moalemi <mohammadmoallemi78@gmail.com>
2024-11-02 09:36:07 +0330

    fix(shared/ui): fix Input component props to work with react hook form

--- src/shared/ui/Input/index.tsx
+++ src/shared/ui/Input/index.tsx
@@ -1,15 +1,24 @@
-import { cn } from "@/lib/utils" 
+import React, { forwardRef } from "react";
+import { cn } from "@/lib/utils";
 
-interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  value?: string;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+}
 
-export function Input({ className, ...props }: InputProps) {
+export const Input = forwardRef<HTMLInputElement, InputProps>(({ className, value, onChange, ...props }, ref) => {
   return (
     <input
+      ref={ref} 
       className={cn(
-        "w-full bg-transparent text-cream  input-border rounded-xl py-3 px-4 caret-cream placeholder-cream placeholder:opacity-50 outline-none",
+        "w-full bg-transparent text-cream input-border rounded-xl py-3 px-4 caret-cream placeholder-cream placeholder:opacity-50 outline-none",
         className 
       )}
+      value={value} 
+      onChange={onChange} 
       {...props}
     />
-  )
-}
+  );
+});
+
+Input.displayName = "Input";
